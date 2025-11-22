163f7e8335951ed8e134ac7b40cbd6ac7f3d5c96
JideMartins <jeedeyy@gmail.com>
2025-11-22 17:50:30 -0500

    Fix errors in functions: - Fixed id having the same description - Fixed updatedAt id and argument type validation

--- main.py
+++ main.py
@@ -6,29 +6,58 @@ import json
 def now():
     return datetime.now().strftime("%d/%m/%Y %H:%M:%S")
 
+
 # JSON handling
 def convert_json(json_string):
     data_dict = json.loads(json_string)
     return data_dict
 
+
+# update data
+def update_task(data_json):
+    print(data_json)
+    data_dict = convert_json(data_json)
+    
+    id = input("what is the id number: ")
+    if str(id) not in data_dict.keys():
+        print("id not found")
+        print(data_dict)
+
+    else:
+        u_input = input(
+            "what would you like to update?\nt for task\ns for status: "
+        ).lower()
+        if u_input == "t":
+            data_dict[id]["description"] = input("update task: ")
+        elif u_input == "d":
+            data_dict[id]["status"] = input("update status(done, in-progress, todeeeo): ")
+        else:
+            print("Command not found")
+    
+    # convert to json
+    data_json = json.dumps(data_dict, indent=4)
+    return data_json
+
+
 # add  data
 def base_data():
     data = {}
-    id = 1
-
-    task = {"description": "", "status": "todo", "createdAt": now(), "updatedAt": None}
+    id = 0
 
     while True:
-        task["description"] = input("input task: ")
-        task["updatedAt"] = now()
-        data[id] = task
         id += 1
+        data[id] = {
+            "description": "",
+            "status": "todo",
+            "createdAt": now(),
+            "updatedAt": None,
+        }
+        data[id]["description"] = input("input task: ")
+        data[id]["updatedAt"] = now()
         user_input = input("Would you like to addmore tasks? y/n: ")
-
         if user_input != "y":
             print("bye!")
             break
-    
 
     # convert to json
     data_json = json.dumps(data, indent=4)
@@ -36,4 +65,5 @@ def base_data():
 
 
 print("=" * 50)
-print(f"Your tasks\n{base_data()}")
+# print(f"Your tasks\n{base_data()}")
+print(f"Your tasks\n{update_task(base_data())}")
