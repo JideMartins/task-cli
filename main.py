e13b665eb4987d49246c6ebd1db943926a5a77a6
JideMartins <jeedeyy@gmail.com>
2025-11-22 15:04:36 -0500

    Create dictionary for storing data

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
-tasks = {}
-
+tasks = {"ID": 0, "description": "", "status": "todo", "createdAt": "", "updatedAt": ""}
+base_tasks = {}
 print("Get your tasks done!")
+
+
+# Add task
+def add_task(data):
+    des = input("Input new task: ")
+
+    data["ID"] += 1
+    data["description"] = des
 
 
 print(tasks)
 num = 0
-is_done = True
-while is_done:
-    task = input("Input new task: ")
-    num += 1
-    tasks[f"00{num}"] = task
+not_done = True
+while not_done:
+    add_task(tasks)
+    base_tasks.update(tasks)
+    
     user_input = input("Would you like to addmore tasks? y/n: ")
     if user_input.lower() == "y":
         continue
     else:
-        is_done = False
+        not_done = False
         print("bye")
 
 print("=" * 50)
-print(f"Your tasks\n{tasks}")
+print(f"Your tasks\n{base_tasks}")
