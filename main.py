64e847829eb88017409208632c24e35872b6b71e
JideMartins <jeedeyy@gmail.com>
2025-11-22 14:37:15 -0500

    Add loop for inputs and populating ID

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 tasks = {}
 
 print("Get your tasks done!")
-task = input("Input new task")
 
-tasks["001"] = task
 
 print(tasks)
+num = 0
+is_done = True
+while is_done:
+    task = input("Input new task: ")
+    num += 1
+    tasks[f"00{num}"] = task
+    user_input = input("Would you like to addmore tasks? y/n: ")
+    if user_input.lower() == "y":
+        continue
+    else:
+        is_done = False
+        print("bye")
 
-
+print("=" * 50)
+print(f"Your tasks\n{tasks}")
