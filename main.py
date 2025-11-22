2a29efd3313a9e6517a3eeea42ba096251fd7d70
JideMartins <jeedeyy@gmail.com>
2025-11-22 15:48:29 -0500

    Clean up

--- main.py
+++ main.py
@@ -6,18 +6,6 @@ def now():
     return datetime.now().strftime("%d/%m/%Y %H:%M:%S")
 
 
-# # Add task
-# def add_task(data):
-#     des = input("Input new task: ")
-
-#     data["ID"] += 1
-#     data["description"] = des
-
-
-# def update_task(data):
-#     data["updatedAt"] = now()
-
-
 # add  data
 def base_data():
     data = {}
