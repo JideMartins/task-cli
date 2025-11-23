95bd73f52f418122721fd261c022e0c7fae93d5b
JideMartins <jeedeyy@gmail.com>
2025-11-23 06:57:36 -0500

    update setup and fix bugs

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="task-cli",
     version="0.1.0",
-    py_modules=["cli"],
+    py_modules=["cli", "file_utils", "task_manager"],
     install_requires=[],
     entry_points={"console_scripts": ["tsk = cli:main"]},
 )
